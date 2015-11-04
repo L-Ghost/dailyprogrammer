4bb18780c519c32c25f7818c2663d16b28a97c79
luisforseti <luis.arantes@forseti.com.br>
2015-11-04 12:40:48 -0200

    EASY 007 - Morse Code Translator
     -> -.-. --- -. ...- . .-. - / .-. --- -- .- -. / .- .-.. .--. .... .- -... . - / - --- / -- --- .-. ... . / -.-. --- -.. .

---
+++ EASY/007 - Morse Code/challenge_007.rb
@@ -0,0 +1,56 @@
+
+class Morse
+  
+  attr_reader :morse_index, :input_string
+  
+  def initialize
+    @morse_index = {'.-' => 'a', '-...' => 'b', '-.-.' => 'c', '-..' => 'd', '.' => 'e', '..-.' => 'f', '--.' => 'g', '....' => 'h', '..' => 'i',
+      '.---' => 'j', '-.-' => 'k', '.-..' => 'l', '--' => 'm', '-.' => 'n', '---' => 'o', '.--.' => 'p', '--.-' => 'q', '.-.' => 'r', '...' => 's',
+      '-' => 't', '..-' => 'u', '...-' => 'v', '.--' => 'w', '-..-' => 'x', '-.--' => 'y', '--..' => 'z', '/' => ' '} # 26 characters + space
+  end
+  
+  def translateMorse # convert morse to letters
+    
+    getCode
+    output_string = ""
+    
+    @input_string.split(" ").each do |l|
+      if (!@morse_index[l])
+        puts "There's an invalid character in your code -> #{l}"
+        output_string += "#"
+      else
+        output_string += @morse_index[l]
+      end
+    end
+    
+    puts output_string
+    
+  end
+  
+  def toMorse # convert letters to morse
+    
+    getCode
+    output_string = ""
+    
+    @input_string.split("").each do |c|
+      if (@morse_index.key(c))
+        output_string += @morse_index.key(c) + " "
+      else
+        puts "No translation found for this character -> #{c}"
+      end
+    end
+    
+    puts output_string
+    
+  end
+  
+  def getCode
+    puts "Insert the code you want to translate."
+    @input_string = gets.chomp
+  end
+  
+end
+
+morse = Morse.new
+morse.translateMorse
+morse.toMorse
