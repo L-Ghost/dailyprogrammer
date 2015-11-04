74c41a22b083fefb257126acd5fc9ffdf801eaaf
luisforseti <luis.arantes@forseti.com.br>
2015-11-04 15:55:49 -0200

    Easy 008 - 99 Bottles of Beer
      - two solutions provided

---
+++ EASY/008 - 99 Bottles of Beer/README.md
@@ -0,0 +1,6 @@
+
+https://www.reddit.com/r/dailyprogrammer/comments/pserp/2162012_challenge_8_easy/
+
+
+"Write a program that will print the song '99 bottles of beer on the wall'.
+For extra credit, do not allow the program to print each loop on a new line."
---
+++ EASY/008 - 99 Bottles of Beer/challenge_008.rb
@@ -0,0 +1,21 @@
+
+output_string = ""
+song = "%i bottle%s of beer on the wall, %i bottle%s of beer\nTake one down, pass it around, %i bottle%s of beer on the wall..."
+
+# extra credit solution
+99.downto(1) do |n|
+  if (n > 1)
+    unless (n == 2)
+      output_string += song % [n, "s", n, "s", n - 1, "s"] + "\n"
+    else
+      output_string += song % [n, "s", n, "s", n - 1, ""] + "\n"
+    end
+  else # n == 1
+    output_string += song % [n, "", n, "", n - 1, "s"] + "\n"
+  end
+end
+
+#print output_string
+
+# single line solution - works if you save two bottles of beer for later
+#99.downto(3).each {|n| print "%i bottles of beer on the wall, %i bottles of beer.\nTake one down, pass it around, %i bottles of beer on the wall..." % [n, n, n - 1] + "\n"}
