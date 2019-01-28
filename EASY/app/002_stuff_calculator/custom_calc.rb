4d70663dee77e967202e2cf38774d5fd4862368a
L-Ghost <luis.arantes.sp@gmail.com>
2019-01-28 16:52:49 -0200

    incrementing tests for EASY 002

--- EASY/app/002_stuff_calculator/custom_calc.rb
+++ EASY/app/002_stuff_calculator/custom_calc.rb
@@ -3,15 +3,15 @@ class CustomCalc
   
   def bodyMassIndex(weight, height)
     bmi = weight / (height * height)
-    return bmi.round(2)
+    bmi.round(2)
   end
   
   def mediumSpeed(distance, time)
-    speed = distance / time
+    (distance / time).round(2)
   end
   
   def triangleArea(base, height)
-    area = (base * height) / 2
+    (base * height) / 2
   end
   
 end
--- EASY/test/spec/002_stuff_calculator.rb
+++ EASY/test/spec/002_stuff_calculator.rb
@@ -2,20 +2,31 @@ require_relative '../../app/002_stuff_calculator/custom_calc.rb'
 
 describe CustomCalc do
   context "When testing the CustomCalc class" do
+    let(:cc) {CustomCalc.new}
     
-    it "should return correct value" do
-      cc = CustomCalc.new()
-
+    it "should return correct body mass index" do
       bmi = cc.bodyMassIndex(100, 2)
       expect(bmi).to eq 25
       bmi2 = cc.bodyMassIndex(88, 1.75)
       expect(bmi2).to eq 28.73
-      
+    end
+
+    it "should return correct medium speed" do
       ms = cc.mediumSpeed(100, 2)
       expect(ms).to eq 50
-      
+      ms2 = cc.mediumSpeed(200, 3)
+      expect(ms2).to eq 66
+      ms3 = cc.mediumSpeed(555, 5)
+      expect(ms3).to eq 111
+    end
+
+    it "should return correct triangle area" do
       ta = cc.triangleArea(6, 8)
       expect(ta).to eq 24
+      ta2 = cc.triangleArea(10, 8)
+      expect(ta2).to eq 40
+      ta3 = cc.triangleArea(40.5, 4)
+      expect(ta3).to eq 81
     end
     
   end
